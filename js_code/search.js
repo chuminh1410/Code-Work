cf9123a25f5a57bec287fbb00d5412e724bbc333
chuminh1410 <chungocquangminh@gmail.com>
2022-11-21 20:58:33 +0700

    1

--- js_code/search.js
+++ js_code/search.js
@@ -2,6 +2,7 @@
 const search = async () => {
 
 
+<<<<<<< Updated upstream
 	const options = {
 		method: 'GET',
 		headers: {
@@ -20,6 +21,26 @@ const search = async () => {
 	songs = data1.albums.items[0].data.artists.items[0].profile
 
 
+=======
+		const options = {
+			method: 'GET',
+			headers: {
+				'X-RapidAPI-Key': '6d0a49ad1amsha8da871af40ca9fp1e3017jsn1f71a1fca7a4',
+				'X-RapidAPI-Host': 'spotify23.p.rapidapi.com'
+			}
+		};
+		var response = ""
+		response = await fetch('https://spotify23.p.rapidapi.com/search/?q=ngoi%20sao%20co%20don&type=multi&offset=0&limit=10&numberOfTopResults=5', options)
+			.then(response => response.json())
+			.then(response => console.log(response))
+			.catch(err => console.error(err));
+		data = await response
+		console.log(data)
+
+		songs = albums.items[0].data.artists.items[0].profile.name
+
+		
+>>>>>>> Stashed changes
 		songs.forEach(element => {
 
 
